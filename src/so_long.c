064dcf22cff166d3f5497bf6acacb6f081ba3496
42stuff <129979364+eescaleira@users.noreply.github.com>
2023-11-23 00:31:34 +0000

    passing image to window

--- src/so_long.c
+++ src/so_long.c
@@ -6,31 +6,26 @@
 /*   By: eescalei <eescalei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 14:39:42 by eescalei          #+#    #+#             */
-/*   Updated: 2023/11/20 18:06:16 by eescalei         ###   ########.fr       */
+/*   Updated: 2023/11/23 00:30:40 by eescalei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/so_long.h"
-
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
+#include <stdio.h>
 
 int main()
 {
 	void *mlx;
 	void *mlx_win;	
-	t_data img;
-
+	void *img;
+	int x = 16, y = 16;
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "so long");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-				
+	mlx_win = mlx_new_window(mlx, 500, 500, "first window");
+	// img = mlx_xpm_file_to_image(mlx, "./assets/big_demon_idle_anim_f0.png", &x, &y);
+	// mlx_put_image_to_window(mlx, mlx_win, img, 0, 0);
+	for(x = 0; x < 500; x++)
+		for(y = 0; y < 500; y++)
+			mlx_pixel_put(mlx, mlx_win, x, y, rand() % 0xFFFFFFFF);
+					
 	mlx_loop(mlx);
 }
