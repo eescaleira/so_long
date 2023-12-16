2726fe3fc9b66f219c7951b90082680298ef43cb
hackerman <hackerman@hackerman>
2023-12-16 15:35:08 +0000

    putting blocks funcs

--- src/innit_free.c
+++ src/innit_free.c
@@ -6,7 +6,7 @@
 /*   By: eescalei <eescalei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 16:51:01 by eescalei          #+#    #+#             */
-/*   Updated: 2023/12/16 15:05:49 by eescalei         ###   ########.fr       */
+/*   Updated: 2023/12/16 15:34:07 by eescalei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,43 +18,44 @@ void innit_window(t_mlx_data *data)
 	data->mlx_win = mlx_new_window(data->mlx, 500, 500, "first window");
 }
 
-void innit_img(void* mlx, t_data *img)
+void innit_img(void* mlx, t_data *img, char *path)
 {
-	// img->img = mlx_new_image(mlx, 500, 500);
-	// img->img_pixels = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
+	img->img = mlx_new_image(mlx, 500, 500);
+	img->img_pixels = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
+	img->img = mlx_xpm_file_to_image(mlx, path, &img->width, &img->height);
 }
 
 
 void create_images(t_mlx_data *data)
 {
-	innit_img(data->mlx, data->imgs->wall);
-	// innit_img(data->mlx, data->imgs->floor);
-	// innit_img(data->mlx, data->imgs->exit);
-	// innit_img(data->mlx, data->imgs->blocked_exit);
-	// innit_img(data->mlx, data->imgs->collectible);
-	// innit_img(data->mlx, data->imgs->player);
-	// data->imgs->wall->img = mlx_xpm_file_to_image(data->mlx, "./assets/wall.xpm", &data->imgs->wall->width, &data->imgs->wall->height);
-	// data->imgs->floor->img = mlx_xpm_file_to_image(data->mlx, "./assets/wall.xpm", &data->imgs->floor->width, &data->imgs->wall->height);
-	// data->imgs->exit->img = mlx_xpm_file_to_image(data->mlx, "./assets/wall.xpm", &data->imgs->exit->width, &data->imgs->exit->height);
-	// data->imgs->blocked_exit->img = mlx_xpm_file_to_image(data->mlx, "./assets/wall.xpm", &data->imgs->blocked_exit->width, &data->imgs->blocked_exit->height);
-	// data->imgs->collectible->img = mlx_xpm_file_to_image(data->mlx, "./assets/wall.xpm", &data->imgs->collectible->width, &data->imgs->collectible->height);
-	// data->imgs->player->img = mlx_xpm_file_to_image(data->mlx, "./assets/wall.xpm", &data->imgs->player->width, &data->imgs->player->height);
+	data->imgs = malloc(sizeof(t_image));
+	data->imgs->wall = malloc(sizeof(t_data));
+	innit_img(data->mlx, data->imgs->wall, "./assets/wall.xpm");
+	data->imgs->floor = malloc(sizeof(t_data));
+	innit_img(data->mlx, data->imgs->floor, "./assets/floor.xpm");
+	data->imgs->exit = malloc(sizeof(t_data));
+	innit_img(data->mlx, data->imgs->exit, "./assets/exit.xpm");
+	data->imgs->blocked_exit = malloc(sizeof(t_data));
+	innit_img(data->mlx, data->imgs->blocked_exit, "./assets/blocked_exit.xpm");
+	data->imgs->collectible = malloc(sizeof(t_data));
+	innit_img(data->mlx, data->imgs->collectible, "./assets/collectible.xpm");
+	data->imgs->player = malloc(sizeof(t_data));
+	innit_img(data->mlx, data->imgs->player, "./assets/player.xpm");
 }
 
 void destroy_images(t_mlx_data *data)
 {
-	mlx_destroy_image(data->mlx, data->imgs->wall);
-	mlx_destroy_image(data->mlx, data->imgs->floor);
-	mlx_destroy_image(data->mlx, data->imgs->exit);
-	mlx_destroy_image(data->mlx, data->imgs->blocked_exit);
-	mlx_destroy_image(data->mlx, data->imgs->collectible);
-	mlx_destroy_image(data->mlx, data->imgs->player);
+	mlx_destroy_image(data->mlx, data->imgs->wall->img);
+	mlx_destroy_image(data->mlx, data->imgs->floor->img);
+	mlx_destroy_image(data->mlx, data->imgs->exit->img);
+	mlx_destroy_image(data->mlx, data->imgs->blocked_exit->img);
+	mlx_destroy_image(data->mlx, data->imgs->collectible->img);
+	mlx_destroy_image(data->mlx, data->imgs->player->img);
 }
 
 int destroy_window(t_mlx_data *data)
 {
-	// destroy_images(data);
-	mlx_destroy_image(data->mlx, data->img.img);
+	destroy_images(data);
 	mlx_destroy_window(data->mlx, data->mlx_win);
 	mlx_destroy_display(data->mlx);
 	free(data->mlx);
