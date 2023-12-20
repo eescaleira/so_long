65a9dd84d88b9a64d67b96763a6fe1bb711a9dc0
hackerman <hackerman@hackerman>
2023-12-20 13:01:29 +0000

    putting blocks funcs

--- src/check_map.c
+++ src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: eescalei <eescalei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 17:20:30 by eescalei          #+#    #+#             */
-/*   Updated: 2023/12/19 23:50:12 by eescalei         ###   ########.fr       */
+/*   Updated: 2023/12/20 13:01:15 by eescalei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,18 @@ void copy_map(char **map, char *map_name)
 		ft_printf("Error openning file\n");
 		exit(0);
 	}
-	// *map = get_next_line(fd);
-	// if(map == NULL)
-	// {
-		// ft_printf("Error\n");
-		// exit(0);
-	// }
-	// temp = *map;
-	// while(map != NULL)
-	// {
-		// **map++;
-		// *map = get_next_line(fd);
-	// }
-	// **map++;
-	// *map = temp;
+	*map = get_next_line(fd);
+	if(map == NULL)
+	{
+		ft_printf("Error\n");
+		exit(0);
+	}
+	temp = *map;
+	while(map != NULL)
+	{
+		**map++;
+		*map = get_next_line(fd);
+	}
+	**map++;
+	*map = temp;
 }
